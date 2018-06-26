4226632f3b3ca0062fe7811dfc01ca7a55e8ede3
wresnikg16 <32616923+wresnikg16@users.noreply.github.com>
2018-06-26 19:01:15 +0200

    SampleServer created, added input function with sending to RabbitMQ queue

--- Server/receive.js
+++ Server/receive.js
@@ -4,7 +4,7 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'newWords';
 
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
