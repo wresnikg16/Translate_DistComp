2cf8a4d5d58c372b36327fa40f8dd00a9487747e
wresnikg16 <32616923+wresnikg16@users.noreply.github.com>
2018-07-17 21:15:11 +0200

    Test from javascript Worker to send message with Socket working

--- Server/receive.js
+++ Server/receive.js
@@ -14,7 +14,40 @@ var db;
 let find_json = [];
 var config = yml.sync('config.yml');
 
-var connection = new WebSocket('ws://10.71.2.233:1337');
+//Websocket
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
+
+function sendResponse(message, server){
+client.on('connectFailed', function(error) {
+  console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+      console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+      console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received: '" + message.utf8Data + "'");
+      }
+  });
+  
+  function sendNumber() {
+      if (connection.connected) {
+          connection.sendUTF(message);
+      }
+  }
+  sendNumber();
+});
+client.connect(server, 'echo-protocol');
+}
+
+sendResponse("test", "ws://localhost:8082/");
 
 //Database
 function init() {
